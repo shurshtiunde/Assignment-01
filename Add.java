e6430aad425360ea63d3954dbd24815e1a416d3d
shurshtiunde <shurshtiunde@gmail.com>
2025-07-29 15:27:54 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,19 @@
+//Addition of 3 numbers
+import java.util.*;
+public class Add
+{
+       
+public static void main(String[] args)
+{
+Scanner input = new Scanner(System.in);
+int x = 10;
+int y = 20;
+int z = 30;
+System.out.println("x:" + x);
+System.out.println("y:" + y);
+System.out.println("z:" + z);
+System.out.println("Addition is: " + (x + y + z));
+System.out.println("Average is: " + (x + y + z)/3);
+}
+
+}
